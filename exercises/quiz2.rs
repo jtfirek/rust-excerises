816d32ad810944de85889501cb96b0db86a6d5e3
Jacob Firek <jacob@interlay.io>
2023-02-09 13:10:03 -0500

    session 2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,7 +18,7 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
+
 
 pub enum Command {
     Uppercase,
@@ -30,11 +30,26 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)> ) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            let mut newString = string.clone();
+            match command {
+                Command::Uppercase => {
+                    output.push(newString.to_uppercase());
+                },
+                Command::Trim => {
+                    output.push(newString.trim().to_string());
+                },
+                Command::Append(a) => {
+                    let iter = *a as i32;
+                    for n in 0..iter {
+                        newString += "bar";
+                    }
+                    output.push(newString)
+                },
+            }
         }
         output
     }
@@ -43,7 +58,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use my_module::transformer;
     use super::Command;
 
     #[test]
