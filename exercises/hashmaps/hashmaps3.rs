2259cd92f994272759746f52e3d2fe99a77782e5
Jacob Firek <jacob@interlay.io>
2023-02-09 12:45:54 -0500

    session 1

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -36,13 +36,31 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
         //handling team 1
-        if !scores.contains_key(&team_1_name) {
-
-        }
-        scores.insert(&team_1_name).goals_scored += team_1_score;
-        scores.insert(&team_1_name).goals_conceded += team_2_score;
-
+        let team = Team {
+            name: team_1_name.clone(),
+            goals_scored: team_1_score,
+            goals_conceded: team_2_score
+        };
         
+        if let Some(entry) = scores.get_mut(&team_1_name) {
+            entry.goals_scored += team_1_score;
+            entry.goals_conceded += team_2_score;
+        } else {
+            scores.insert(team_1_name.clone(), team);
+        }
+
+        let team = Team {
+            name: team_2_name.clone(),
+            goals_scored: team_2_score,
+            goals_conceded: team_1_score
+        };
+        
+        if let Some(entry) = scores.get_mut(&team_2_name) {
+            entry.goals_scored += team_2_score;
+            entry.goals_conceded += team_1_score;
+        } else {
+            scores.insert(team_2_name.clone(), team);
+        }
     }
     scores
 }
